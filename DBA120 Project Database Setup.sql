808df9e648cc6278943a3100070e877ba1f09a5c
awkwardman <122575205+awkwardman@users.noreply.github.com>
2023-04-17 16:08:15 -0400

    Add files via upload

--- DBA120 Project Database Setup.sql
+++ DBA120 Project Database Setup.sql
@@ -3,11 +3,12 @@ CREATE DATABASE ketoPilot;
 CREATE TABLE ketoPilot.customers
     (
         accountNum      INT             PRIMARY KEY     NOT NULL    AUTO_INCREMENT,
+        email           VARCHAR(50)     NOT NULL        UNIQUE,
         lastName        VARCHAR(50)     NOT NULL,
         firstName       VARCHAR(50)     NOT NULL,
         city            VARCHAR(50)     NOT NULL,
-        state           VARCHAR(50)     NOT NULL,
-        zipCode         VARCHAR(50)     NOT NULL,
+        state           VARCHAR(2)      NOT NULL,
+        zipCode         INT(5)          NOT NULL,
         country         VARCHAR(50)     NOT NULL
     );
 
@@ -55,3 +56,39 @@ CREATE TABLE ketoPilot.userProfiles
         BSTestID        INT             REFERENCES bloodspotData (BSTestID),
         BRKetoTestID    INT             REFERENCES breathKetones (BRKetoTestID)
     );
+
+INSERT INTO customers VALUES
+    (DEFAULT,'michaeljchapman@students.abtech.edu','Chapman','Michael','Asheville','NC',28803,'USA'),
+    (DEFAULT,'madeup@students.abtech.edu','Donald','Ashley','Asheville','NC',28803,'USA');
+
+INSERT INTO accountSecurity VALUES
+    ('michaeljchapman@students.abtech.edu',(SELECT accountNum FROM customers WHERE email='michaeljchapman@students.abtech.edu'),'indiana',12345),
+    ('madeup@students.abtech.edu',(SELECT accountNum FROM customers WHERE email='madeup@students.abtech.edu'),'southcarolina',67890);
+
+
+
+    import csv
+import mysql.connector
+
+# Connect to MySQL database
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="yourdatabase"
+)
+
+# Open the CSV file and read the data
+with open('data.csv', 'r') as file:
+    reader = csv.reader(file)
+    next(reader)  # skip header row
+    for row in reader:
+        date = row[0]
+        result = row[1]
+        
+        # Insert the data into the MySQL table
+        cursor = mydb.cursor()
+        sql = "INSERT INTO yourtable (date, result) VALUES (%s, %s)"
+        val = (date, result)
+        cursor.execute(sql, val)
+        mydb.commit()
